618dfa76e7e80188264257f23c5d2fddca99a4ff
NaphatBur <m_moszaza@hotmail.com>
2016-05-16 08:12:51 +0700

    update 03

--- FabMenu.java
+++ FabMenu.java
@@ -2,6 +2,7 @@ package com.app.ordering.orderingsystem;
 
 import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 
 import com.gordonwong.materialsheetfab.AnimatedFab;
 
@@ -14,7 +15,17 @@ public class FabMenu extends FloatingActionButton implements AnimatedFab {
     }
 
     @Override
-    public void show(float translationX, float translationY) {
+    public void show() {
+        show(0,0);
+    }
 
+    @Override
+    public void show(float translationX, float translationY) {
+        setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hide() {
+        setVisibility(View.INVISIBLE);
     }
 }
