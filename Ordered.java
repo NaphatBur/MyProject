2e2e3cdc61c63bcfba17040536b9b01a1e01877d
NaphatBur <m_moszaza@hotmail.com>
2016-05-17 02:18:29 +0700

    update Ver02

--- Ordered.java
+++ Ordered.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -25,6 +26,11 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import AsyncTask.AsyncTaskCategory;
+import it.gmariotti.cardslib.library.cards.actions.BaseSupplementalAction;
+import it.gmariotti.cardslib.library.cards.actions.TextSupplementalAction;
+import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.view.CardViewNative;
 
 
 /**
@@ -91,6 +97,7 @@ public class Ordered extends Fragment {
                             @Override
                             public void run() {
                                 setCardViewFromCategory(countCate);
+                                //setCustomCard();
                             }
                         });
 
@@ -100,7 +107,9 @@ public class Ordered extends Fragment {
     }
 
     private void setCardViewFromCategory(int countCate){
-        CardView[] cardCate = new CardView[countCate];
+        //CardView[] cardCate = new CardView[countCate];
+        CardViewNative[] cardCate = new CardViewNative[countCate];
+        Card[] card = new Card[countCate];
         TextView[] txtCateName = new TextView[countCate];
         LinearLayout[] layoutFoodCard = new LinearLayout[countCate];
         ImageView[] imgCate = new ImageView[countCate];
@@ -111,21 +120,22 @@ public class Ordered extends Fragment {
         );
         params.gravity = Gravity.CENTER;
 
-        LinearLayout.LayoutParams paramsLayoutFood = new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams paramsLayoutCate = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         );
 
         for (int i=0;i<countCate;i++){
-            cardCate[i] = new CardView(context);
-            cardCate[i].setLayoutParams(new RelativeLayout.LayoutParams(100, 100));
+            cardCate[i] = new CardViewNative(context);
+            cardCate[i].setLayoutParams(new RelativeLayout.LayoutParams(100, 85));
             cardCate[i].setRadius(4);
-            cardCate[i].setContentPadding(10,10,10,8);
+            cardCate[i].setContentPadding(0,0,0,0);
             cardCate[i].setCardElevation(9);
 
+            card[i] = new Card(context);
 
             layoutFoodCard[i] = new LinearLayout(context);
-            layoutFoodCard[i].setLayoutParams(paramsLayoutFood);
+            layoutFoodCard[i].setLayoutParams(paramsLayoutCate);
             layoutFoodCard[i].setOrientation(LinearLayout.VERTICAL);
 
             imgCate[i] = new ImageView(context);
@@ -140,19 +150,24 @@ public class Ordered extends Fragment {
             txtCateName[i].setLayoutParams(params);
             txtCateName[i].setText(cateName.get(i));
             txtCateName[i].setTextSize(11);
+            txtCateName[i].setPadding(0,3,0,0);
+            txtCateName[i].setGravity(Gravity.CENTER);
 
+            card[i].setOnClickListener(new setOnCardClicked(i));
+
+            cardCate[i].setCard(card[i]);
             layoutFoodCard[i].addView(imgCate[i]);
             layoutFoodCard[i].addView(txtCateName[i]);
-            cardCate[i].addView(layoutFoodCard[i]);
 
+            cardCate[i].addView(layoutFoodCard[i]);
 
             layout_food.addView(cardCate[i]);
 
-            cardCate[i].setOnClickListener(new setOnCardClicked(i));
         }
     }
 
-    private class setOnCardClicked implements View.OnClickListener{
+
+    private class setOnCardClicked implements Card.OnCardClickListener{
 
         int position;
 
@@ -161,9 +176,74 @@ public class Ordered extends Fragment {
         }
 
         @Override
-        public void onClick(View v) {
+        public void onClick(Card card, View view) {
             Log.d("position", String.valueOf(position));
+
         }
+
+    }
+
+    private void setFoodLayout(int position){
+        LinearLayout[] lFood = new LinearLayout[cateName.size()];
+
+        LinearLayout.LayoutParams paramsLayoutFood = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+
+        for(int i=0; i<cateName.size(); i++){
+            lFood[i] = new LinearLayout(context);
+            lFood[i].setLayoutParams(paramsLayoutFood);
+            lFood[i].setOrientation(LinearLayout.HORIZONTAL);
+        }
+    }
+
+    private void setCustomCard(){
+
+        CardViewNative[] cardCate = new CardViewNative[countCate];
+
+        final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        //params.gravity = Gravity.CENTER;
+
+        cardCate[0] = new CardViewNative(context);
+        cardCate[0].setLayoutParams(params);
+        cardCate[0].setRadius(4);
+        //cardCate[i].setContentPadding(10,10,10,8);
+        cardCate[0].setCardElevation(9);
+
+
+
+        final ImageView[] imgCate = new ImageView[countCate];
+        imgCate[0] = new ImageView(context);
+        imgCate[0].setLayoutParams(params);
+
+        MaterialLargeImageCard card = MaterialLargeImageCard.with(context)
+                .setTextOverImage(cateName.get(0))
+                .setTitle(cateName.get(0))
+                .setSubTitle(cateName.get(0))
+                .useDrawableExternal(new MaterialLargeImageCard.DrawableExternal() {
+                    @Override
+                    public void setupInnerViewElements(ViewGroup parent, View viewImage) {
+                        Picasso.with(getActivity()).setIndicatorsEnabled(true);  //only for debug tests
+                        Picasso.with(getActivity())
+                                .load("https://lh5.googleusercontent.com/-squZd7FxR8Q/UyN5UrsfkqI/AAAAAAAAbAo/VoDHSYAhC_E/s96/new%2520profile%2520%25282%2529.jpg")
+                                .into((ImageView) viewImage);
+                    }
+                })
+                .build();
+
+        card.setOnClickListener(new Card.OnCardClickListener() {
+            @Override
+            public void onClick(Card card, View view) {
+                Toast.makeText(getActivity()," Click on ActionArea ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cardCate[0].setCard(card);
+        layout_food.addView(cardCate[0]);
     }
 
 }
